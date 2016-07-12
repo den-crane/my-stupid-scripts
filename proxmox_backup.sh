89eb4fb5ea2d21a2104acc7d9934f2093017371f
Denis Zhuravlev <deniszhuravlev@Deniss-MacBook-Air.local>
2016-07-12 17:33:36 -0300

    aws expected-size added for huge files, not exit from loop if vzdump failed

--- proxmox_backup.sh
+++ proxmox_backup.sh
@@ -18,31 +18,51 @@ pvVer=`pveversion|cut -c 1-15`
 if [ "$pvVer" == "pve-manager/3.4" ]
 then
   cntrCntZero=`(vzlist 2>&1|grep -c "Container(s) not found")||true`
-  if [ $cntrCntZero -eq 1 ]
+  if [ $cntrCntZero -eq 1 ]  
      then
-       vmlist=`qm list|awk '$3 == "running" {print $1"|"$2"|qemu"}'`
+       vmlist=`qm list|awk '$3 == "running" {print $1"|"$2"|qemu"}'` 
      else
        vmlist=`vzlist |awk '$3 == "running" {print $1"|"$5"|openvz"}';qm list|awk '$3 == "running" {print $1"|"$2"|qemu"}'`
   fi
-else
-  vmlist=`qm list|awk '$3 == "running" {print $1"|"$2"|qemu"}'`
+else 
+  vmlist=`qm list|awk '$3 == "running" {print $1"|"$2"|qemu"}'`  
 fi
 
-echo "Backup start `date`"
+vmcnt=0
+for vm in $vmlist;
+do
+  ((vmcnt+=1))
+done
+
+echo "$vmcnt running VM(s) found. Backup start `date`"
+echo ""
+
+bkpcnt=0
 for vm in $vmlist; 
 do 
     id=`echo $vm|awk -F"|" '{print $1}'`
     vmname=`echo $vm|awk -F"|" '{print $2}'`
     type=`echo $vm|awk -F"|" '{print $3}'`
+    echo ""
+    echo "*******************************************************************************************"
     awsfile=$targetdir/vzdump-$type-$id-$vmname.vma.gz
     echo "Backup of $id - $vmname ($type) to $awsfile started."
-    /usr/bin/vzdump $id -mode snapshot --dumpdir $tmpdir --compress gzip --quiet --stdout|pv -q --rate-limit $uploadspeed|$aws s3 cp - $awsfile
+    set +e
+    /usr/bin/vzdump $id -mode snapshot --dumpdir $tmpdir --compress gzip --quiet --stdout|pv -q --rate-limit $uploadspeed|$aws s3 cp - $awsfile --expected-size=100000000000
+    exitcode=$?
+    set -e
+    if [ "$exitcode" -ne 0 ]
+        then 
+          message="******* FAIL *******"
+        else 
+          message="######## OK ########" 
+          ((bkpcnt+=1))
+    fi  
     echo ""
-    echo "**************************************************"
-    echo "Backup of $id - $vmname completed. ########### OK."
-    echo "**************************************************"
+    echo "Backup of $id - $vmname completed. $message."
+    echo "*******************************************************************************************"
     echo ""
 done
-echo "Backup complete `date`"
+echo "$bkpcnt VM(s) of $vmcnt successfully backed up. Backup complete `date`"
 ) 2>&1) | tee $logfile
 
